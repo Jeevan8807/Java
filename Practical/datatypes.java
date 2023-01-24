d6ac9ee41f50dd37f46654294c8fdecabeacd49c
Jeevan8807 <123078281+Jeevan8807@users.noreply.github.com>
2023-01-24 18:25:51 +0530

    Add files via upload

---
+++ Practical/datatypes.java
@@ -0,0 +1,21 @@
+
+public class datatypes {
+
+
+	public static void main(String[] args) {
+		display d1= new display();
+			d1.sum(1,4);
+	}
+
+}
+class display{
+
+int sum(int num1, int num2)
+{
+    
+    return num1+num2;
+}
+	
+} 
+    
+
