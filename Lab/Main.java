edf4ccc83ef2443b7250b5fd7a02fa9342269752
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-03 13:04:18 +0530

    Add files via upload

--- Lab/Main.java
+++ Lab/Main.java
@@ -1,45 +1,25 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-       char Operator;
-       Double number1, number2, result;
-
-       Scanner sc= new Scanner(System.in);
-        System.out.println("Choose an operator: +,-,*, or /");
-        Operator = sc.next().charAt(0);
-
-        System.out.println("Enter the First Number");
-        number1 = sc.nextDouble();
-
-        System.out.println("Enter the Second Number");
-        number2 = sc.nextDouble();
-
-        switch (Operator)
+        int sum=0;
+        for (int i = 1; i <= 200; i++)
         {
-            case '+':
-                result = number1 + number2;
-                System.out.println(number1 + " + " + number2+ " = " + result);
-                break;
-
-            case '-':
-                result = number1 - number2;
-                System.out.println(number1 + " - " + number2+ " = " + result);
-                break;
-            case '*':
-                result = number1 * number2;
-                System.out.println(number1 + " * " + number2+ " = " + result);
-                break;
-            case '/':
-                result = number1 / number2;
-                System.out.println(number1 + " / " + number2+ " = " + result);
-                break;
-
-            default:
-                System.out.println("Invalid Operator!");
-                break;
+            if (i%2==0)
+            System.out.println(i);
         }
-
-
+        {
+            for (int i = 1; i <= 200; i++)
+            {
+                if (i%2!=0)
+                    System.out.println(i);
+            }
+            {
+                for (int i = 1; i <= 200; i++)
+                {
+                    sum= sum+i;
+                }
+            }
+            System.out.println(sum);
+        }
     }
 }
