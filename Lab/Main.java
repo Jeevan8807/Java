7a53134baa95fe0cffcef7b6abee5d385609917a
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-03 13:01:18 +0530

    Add files via upload

--- Lab/Main.java
+++ Lab/Main.java
@@ -1,25 +1,10 @@
 public class Main {
     public static void main(String[] args)
     {
-        int sum=0;
-        for (int i = 1; i <= 200; i++)
-        {
-            if (i%2==0)
-            System.out.println(i);
-        }
-        {
-            for (int i = 1; i <= 200; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                if (i%2!=0)
-                    System.out.println(i);
+               if (i%5==0 && i>40)
+                System.out.println(i);
             }
-            {
-                for (int i = 1; i <= 200; i++)
-                {
-                    sum= sum+i;
-                }
-            }
-            System.out.println(sum);
-        }
     }
 }
