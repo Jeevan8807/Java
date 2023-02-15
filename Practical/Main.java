c9a5040abc45a7387d77a41a5f8d5f097a0d0546
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-15 19:17:48 +0530

    Basic Enum

--- Practical/Main.java
+++ Practical/Main.java
@@ -1,38 +1,14 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args)
-    {
-        Scanner Sc =new Scanner(System.in);
-        System.out.println("Enter Your Marks: ");
-        int marks= Sc.nextInt();
-
-                if(marks<35){
-                    System.out.println("Failed!!!");
-                }
-                else if(marks>=35 && marks<40) {
-                    System.out.println("E grade");
-                }
-                else if(marks>=40 && marks<50) {
-                    System.out.println("D grade");
-                }
-                else if(marks>=50 && marks<60){
-                    System.out.println("C grade");
-                }
-                else if(marks>=60 && marks<70){
-                    System.out.println("B grade");
-                }
-                else if(marks>=70 && marks<80){
-                    System.out.println("A grade");
-                }
-                else if(marks>=80 && marks<90){
-                    System.out.println("A+ grade");
-                }else if(marks>=90 && marks<100){
-                    System.out.println("O grade");
-                }else{
-                    System.out.println("Invalid!");
-                }
-            }
-
-        }
+enum cars {
+	XUV,
+	SUV,
+	Sedan,
+	Hatchback
+  }
+  
+  public class Main { 
+	public static void main(String[] args) { 
+	  cars myVar = cars.XUV; 
+	  System.out.println(myVar); 
+	} 
+  }
+  
