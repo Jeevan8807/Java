1c64f52195806b7c86e136995ebfdffa6d1b5975
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-16 19:59:55 +0530

    Add files via upload

---
+++ Practical/typecasting.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class typecasting {
+    public static void main(String[] args) {
+        int num = 10; //create a int.
+        System.out.println("The Integer value: " + num);
+
+        float data = num;// convert into float type
+        System.out.println("The Float value: "+ data);
+
+        
+        
+    }
+    
+}
