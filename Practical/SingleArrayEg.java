e3fe6b14fd403c0051cbcf8d36b5127385d2c67d
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-17 18:30:41 +0530

    Add files via upload

---
+++ Practical/SingleArrayEg.java
@@ -0,0 +1,36 @@
+public class SingleArrayEg { 
+
+    public static void main(String[]args) {
+
+        int myarray[] = new int [4]; // declare the array
+
+        myarray[0]=78; // initialization 
+        myarray[1]=97;
+        myarray[2]=76;
+        myarray[3]=30;
+
+        System.out.println(myarray[2]);// value of index2 
+
+        for (int i=0; i<myarray.length;i++) //length of array
+        System.out.println(myarray[i]);
+
+
+
+        System.out.print("Array of elements: "); // print elements
+        float num[] = {3.3f , 5.8f, 6.2f, 8.8f}; //declare the array , instantiation ,and initialization 
+
+        for(float j : num){  //for loop
+            System.out.print(j+" ,");
+
+                                  
+                                                           // 2 types to print this method 
+           // for(int j =0; j<num.length; j++){   
+             //   System.out.println(num[j]+",");
+            }
+
+            
+        }
+
+        
+    }
+    
