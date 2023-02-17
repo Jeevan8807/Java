f951741ded776b6fced00dc412ea76a87bf742a7
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-17 18:31:05 +0530

    Add files via upload

---
+++ Practical/MultiArrayEg.java
@@ -0,0 +1,19 @@
+public class MultiArrayEg {
+
+    public static void main(String []args) {
+         int arr1[][] ={{2,9,8,},{1,2,3},{7,8,9}};
+
+
+         for(int i =0; i<arr1.length;i++){
+            for(int j =0; j<arr1[i].length;j++){
+                System.out.print(arr1[i][j]);
+
+            }
+            System.out.println();
+         }
+
+         
+        
+    }
+    
+}
