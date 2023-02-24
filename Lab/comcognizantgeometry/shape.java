459bb7aed5619d018940b601a5254be5ad04694c
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-02-24 14:08:06 +0530

    Add files via upload

---
+++ Lab/comcognizantgeometry/shape.java
@@ -0,0 +1,10 @@
+package comcognizantgeometry;
+import com.cognizant.shapes.Circle;//imports a class Circle from a package com.cognizant.shapes
+
+    public class shape {
+
+        public static void main(String[] args) {// main()
+            //		Circle c= new Circle(); // declares an object with value of pi set to 1.5
+            
+        }
+}
---
+++ Lab/comcognizantshaspes/Area.java
@@ -0,0 +1,10 @@
+package comcognizantshaspes;
+
+public class Area {
+
+    public static void main(String[] args) {//main()
+		Circle c= new Circle(3.5f);//sets the pi value to 3.5
+
+    
+}
+}
