5a48f7fe30680a36f0d451968772924b0a50ca3f
Jeevan Harishchandra Darekar <123078281+Jeevan8807@users.noreply.github.com>
2023-03-03 09:58:59 +0530

    Update CalculatorLab.java

--- Lab/CalculatorLab.java
+++ Lab/CalculatorLab.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
-public class CalculatorLab {
+public class CalculatorLab { //create a class 
     
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int option;
+    public static void main(String args[]) { //create a main method 
+        Scanner sc = new Scanner(System.in); // add class and create a class of object 
+        int option;. // Add a variable 
         do{
-            System.out.println("Enter a value:\n +"+"1-Add\n"+"2-Substract\n"+
+            System.out.println("Enter a value:\n +"+"1-Add\n"+"2-Substract\n"+ //print all the menu   
             "3-Multiply\n"+
             "4-Even/Odd number\n"
             +"5-Quit");
     
-option=sc.nextInt();
-switch(option) {
+option=sc.nextInt(); // to take a input from user
+switch(option) { //to select a menu in different case
 case 1:
-    System.out.println("Enter two numbers to be added");
-    double num1=sc.nextInt();
+    System.out.println("Enter two numbers to be added"); //print add
+    double num1=sc.nextInt(); //add two num and read 
     double num2=sc.nextInt();
-    double result= num1+num2;
-    System.out.println("The result is "+ result);
+    double result= num1+num2;. //this line will give the result.
+    System.out.println("The result is "+ result); //print result
     
 case 2:
     System.out.println("Enter two numbers to be substracted");
